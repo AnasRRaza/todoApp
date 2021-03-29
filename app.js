6f5e4508edc438d8bd127184a8aed8ad7fe5c736
Anas Raza <79412884+AnasRRaza@users.noreply.github.com>
2021-03-29 13:49:42 +0500

    Adding app

---
+++ app.js
@@ -0,0 +1,88 @@
+
+//     SCRIPT FOR LOGIN PAGE
+
+function getInput() {
+    let textValue = document.getElementById("text").value;
+    let emailValue = document.getElementById("email").value;
+    localStorage.setItem("Text Value", textValue);
+    localStorage.setItem("Email Value", emailValue);
+}
+// let emailValue = document.getElementById("email");
+let userEmail = document.getElementById("user-email");
+
+userEmail.innerText = localStorage.getItem("Email Value");
+
+function logout() {
+    localStorage.clear();
+}
+
+
+//     SCRIPT FOR HOME PAGE
+function getAndUpdate() {
+    console.log("Adding...")
+    tit = document.getElementById("title").value;
+    des = document.getElementById("description").value;
+    if (localStorage.getItem("itemsJson") == null) {
+        itemsJsonArray = [];
+        itemsJsonArray.push([tit, des]);
+        localStorage.setItem("itemsJson", JSON.stringify(itemsJsonArray));
+    }
+    else {
+        itemsJsonArrayStr = localStorage.getItem("itemsJson");
+        itemsJsonArray = JSON.parse(itemsJsonArrayStr);
+        itemsJsonArray.push([tit, des]);
+        localStorage.setItem("itemsJson", JSON.stringify(itemsJsonArray));
+    }
+    update()
+}
+
+function update() {
+    if (localStorage.getItem("itemsJson") == null) {
+        itemsJsonArray = [];
+        localStorage.setItem("itemsJson", JSON.stringify(itemsJsonArray));
+    }
+    else {
+        itemsJsonArrayStr = localStorage.getItem("itemsJson");
+        itemsJsonArray = JSON.parse(itemsJsonArrayStr);
+    }
+    // Display the Table
+
+    let tableBody = document.getElementById("tbody");
+    let str = "";
+    itemsJsonArray.forEach((element, index) => {
+        str += `
+        <tr>
+            <th>${index + 1}</th>
+            <td>${element[0]}</td>
+            <td>${element[1]}</td>
+            <td><button onclick = "deleted(${index})">Delete</button></td>
+        </tr>        `
+    });
+    tableBody.innerHTML = str;
+}
+
+let add = document.getElementById("add");
+add.addEventListener("click", getAndUpdate);
+update();
+
+function deleted(itemIndex) {
+    console.log("Deleting..", itemIndex);
+    itemsJsonArrayStr = localStorage.getItem("itemsJson")
+    itemsJsonArray = JSON.parse(itemsJsonArrayStr);
+    // Delete the itemIndex element from the array
+    itemsJsonArray.splice(itemIndex, 1);
+    localStorage.setItem("itemsJson", JSON.stringify(itemsJsonArray));
+    update();
+}
+
+let clearList = () => {
+    if (confirm("Do you really want to clear?"))
+        localStorage.removeItem("itemsJson");
+    update();
+}
+
+
+
+
+
+
